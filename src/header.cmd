258d10cb6fecf58dd8a3b2191bd69f74c775ed16
Jonathan <jonathan.max.annett@gmail.com>
2023-04-25 18:54:51 +1000

    Delete header.cmd

--- src/header.cmd
+++
@@ -1,3 +0,0 @@
-@echo off & (For /F Delims^= %%a In ('CertUtil -HashFile %0 SHA1^|FindStr /VRC:"[^a-f 0-9]"') Do Set "PS1=%TEMP%\%%a.ps1" )
-(if not exist %PS1% more +3 %0 > %PS1%) & (PowerShell.exe -ExecutionPolicy bypass -file %PS1% %* & goto :EOF)
-@@@@@@[ PowerShell Starts Here ]@@@@@@
